ca48ac95447dfda70bc7b23ab4e2e3aa41c23b48
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-12-01 19:47:21 +0530

    Add logo and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,72 @@
 import { Link } from "react-scroll";
+import logo from "../assets/images/Logo.png";
 
 const Header = () => {
   return (
     <div className="fixed w-full bg-primary-color z-10">
-      <nav className="flex justify-end mr-10 py-5">
-        <ul className="flex mt-2">
-          <li className="mr-5 " style={{ cursor: "pointer" }}>
-            <Link
-              to="about"
-              className="text-light-color hover:text-secondary-color"
-              spy={true}
-              smooth={true}
-            >
-              <span className="font-monospace text-secondary-color">01. </span>
-              About
-            </Link>
-          </li>
-          <li className="mr-5" style={{ cursor: "pointer" }}>
-            <Link
-              to="experience"
-              className="text-light-color hover:text-secondary-color"
-              spy={true}
-              smooth={true}
-            >
-              <span className="font-monospace text-secondary-color">02. </span>
-              Experience
-            </Link>
-          </li>
-          <li className="mr-5" style={{ cursor: "pointer" }}>
-            <Link
-              to="work"
-              className="text-light-color hover:text-secondary-color"
-              spy={true}
-              smooth={true}
-            >
-              <span className="font-monospace text-secondary-color">03. </span>
-              Work
-            </Link>
-          </li>
-          <li className="mr-5" style={{ cursor: "pointer" }}>
-            <Link
-              to="contact"
-              className="text-light-color hover:text-secondary-color"
-              spy={true}
-              smooth={true}
-            >
-              <span className="font-monospace text-secondary-color">04. </span>
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <div
-          className="rounded-md border-2 border-secondary-color py-2 px-5 text-secondary-color hover:bg-secondary-color hover:text-black"
-          style={{ cursor: "pointer" }}
-        >
-          Resume
+      <nav className="flex justify-between px-10 py-5">
+        <img src={logo} alt="logo" className="h-10" />
+        <div className="flex">
+          <ul className="flex mt-2">
+            <li className="mr-5 " style={{ cursor: "pointer" }}>
+              <Link
+                to="about"
+                className="text-light-color hover:text-secondary-color"
+                spy={true}
+                smooth={true}
+              >
+                <span className="font-monospace text-secondary-color">
+                  01.{" "}
+                </span>
+                About
+              </Link>
+            </li>
+            <li className="mr-5" style={{ cursor: "pointer" }}>
+              <Link
+                to="experience"
+                className="text-light-color hover:text-secondary-color"
+                spy={true}
+                smooth={true}
+              >
+                <span className="font-monospace text-secondary-color">
+                  02.{" "}
+                </span>
+                Experience
+              </Link>
+            </li>
+            <li className="mr-5" style={{ cursor: "pointer" }}>
+              <Link
+                to="work"
+                className="text-light-color hover:text-secondary-color"
+                spy={true}
+                smooth={true}
+              >
+                <span className="font-monospace text-secondary-color">
+                  03.{" "}
+                </span>
+                Work
+              </Link>
+            </li>
+            <li className="mr-5" style={{ cursor: "pointer" }}>
+              <Link
+                to="contact"
+                className="text-light-color hover:text-secondary-color"
+                spy={true}
+                smooth={true}
+              >
+                <span className="font-monospace text-secondary-color">
+                  04.{" "}
+                </span>
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div
+            className="rounded-md border-2 border-secondary-color py-2 px-5 text-secondary-color hover:bg-secondary-color hover:text-black"
+            style={{ cursor: "pointer" }}
+          >
+            Resume
+          </div>
         </div>
       </nav>
     </div>
